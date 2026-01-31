c058ab2259f664ba71e00b2fe49b8fb2304a6c8e
PhanPhong13112005 <luuphongphan12@gmail.com>
2026-01-31 07:43:14 +0000

    up file

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  // Thay đổi dòng này thành domain thật của bạn
+  // static const String baseUrl = "http://10.0.2.2:5000/api"; // Cũ
+  static const String baseUrl = "luuphong-cntt1708.ddns.net/api"; // Mới (Production)
+}
